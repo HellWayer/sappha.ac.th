ddbfe537fedef3416db730ca3bc064d7936afb22
HellWayer <110084751+HellWayer@users.noreply.github.com>
2022-09-14 12:18:56 +0700

    Create C-Test.c

---
+++ C-Test.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+int main() {
+  char locker[20]
+  int pass[10]
+  float fl[10]
+
+  printf('ชื่อไร: ');
+  scanf("%s",locker);
+  
+  printf('เลขที่ไร:');
+  scanf("%i",pass);
+  
+  printf('');
+}
